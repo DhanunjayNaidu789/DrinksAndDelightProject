f9ecb7d3fbea3cddd8d9bd85c8e2e5871aa8f95e
Dhanunjay Naidu <dhanusandy789@gmail.com>
2020-04-26 13:07:32 +0530

    added sridhar's module

---
+++ SridharModule/src/main/java/com/capgemini/main/dao/ProductDaoInterface.java
@@ -0,0 +1,23 @@
+package com.capgemini.main.dao;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+import com.capgemini.main.entites.StockDetails;
+
+public interface ProductDaoInterface {
+	
+	public StockDetails getProductSpecs(String stockId);
+	
+	public List<StockDetails> reterive();
+
+	
+
+	public StockDetails updateProduct(String stockId, StockDetails details);
+
+	
+
+	
+
+}
